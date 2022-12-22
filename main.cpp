37b269e331eb3be82b6d49eea438a6d505b787df
mattianatale <117930083+mattianatale@users.noreply.github.com>
2022-12-22 16:27:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include"math.h"
 using namespace std;
+float f(float y){
 
-int main() {
-   return 0;
+    float risultato = pow(y, 2)* cos(y)+1;
+    return risultato;
 }
+    int main() {
+        float a;
+        float b;
+        do {
+            cout << "inserire estremi" << endl;
+            cin >> a;
+            cin >> b;
+
+        } while (f(a) * f(b) >= 0);
+        float x, error;
+        do {
+            x = (a + b) / 2;
+            if (f(x) == 0) {
+                cout << x;
+                cout << f(x);
+                return 0;
+            } else {
+                if (f(a) * f(b) < 0) {
+                    b = x;
+                } else {
+                    a = x;
+                }
+            }
+            error = abs((b-a)/2);
+        }
+         while (error>=1e-6);
+        cout<<int(x*10000)/10000.0<<endl;
+        return 0;
+    }
