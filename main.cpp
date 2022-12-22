3259159a2a9557f28ba1fcbb10be086dd3915b23
mattianatale <117930083+mattianatale@users.noreply.github.com>
2022-12-22 16:33:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
-#include"math.h"
-using namespace std;
-float f(float y){
+#include "math.h"
+using namesoace std;
 
-    float risultato = pow(y, 2)* cos(y)+1;
+float f(float y){
+    float risultato;
+    risultato = pow(y, 2)* cos(y)+1;
     return risultato;
 }
 
-    int main() {
-        float a;
-        float b;
-        do {
-            cout << "inserire estremi" << endl;
-            cin >> a;
-            cin >> b;
 
-        } while (f(a) * f(b) >= 0);
-        float x, error;
-        do {
-            x = (a + b) / 2;
-            if (f(x) == 0) {
-                cout << x;
-                cout << f(x);
-                return 0;
-            } else {
-                if (f(a) * f(b) < 0) {
-                    b = x;
-                } else {
-                    a = x;
-                }
-            }
-            error = abs((b-a)/2);
+
+int main() {
+    float a, b;
+
+    do{
+        cout << "inserire estremi" << endl;
+        cin >> a >> b;
+        }while(f(a)*f(b)>=0);
+    float x, err;
+
+    do{
+        x=(a+b)/2;
+
+        if(f(x)==0){
+            cout<< x<< endl;
+            cout << f(x);
+            return 0;
         }
-         while (error>=1e-6);
-        cout<<int(x*10000)/10000.0<<endl;
+        else{
+            if(f(a)*f(x)<0){
+                b=x;
+            }
+            else {
+                a=x;
+            }
+            err=abs((b-a)/2);
+        }
+  } while (err>=1e-6);
+    cout<<int(x*10000)/10000.0<<endl;
+
         return 0;
      }
